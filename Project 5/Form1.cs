080c8bcc44e489b2edad1803430123f9a83c16ad
Yolanda Limani <limaniyolanda@gmail.com>
2024-10-18 11:35:34 +0200

    Add project files.

---
+++ Project 5/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_5
+{
+    public partial class Form1 : Form
+    {
+        //private ErrorProvider errorProvider;
+        public Form1()
+        {
+            InitializeComponent();
+            //errorProvider = new ErrorProvider();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();
+            newForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 newForm = new Form3();
+            newForm.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form4 newForm = new Form4();
+            newForm.Show();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();
+            newForm.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form5 newForm = new Form5();
+            newForm.Show();
+
+        }
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string email = textBox1.Text;
+            if (IsValidEmail(email))
+            {
+                textBox1.BackColor = Color.White;
+                
+            }
+            else
+            {
+                //errorProvider.SetError(textBox1, "Enter valid email");
+                textBox1.BackColor = Color.LightPink;
+            }
+        }
+    }
+}
---
+++ Project 5/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_5
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0) {
+                MessageBox.Show("Currency in Rands is: R" + double.Parse(textBox1.Text) * 17.27);
+            }
+            else
+            {
+                MessageBox.Show("Currency in USD is: $" + double.Parse(textBox1.Text) / 17.27);
+            }
+        }
+    }
+}
